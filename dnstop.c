f3be56eaac51fd395a0a223df48d9fb4f73681ee
Duane Wessels <wessels@measurement-factory.com>
2003-09-29 22:48:39 +0000

    We must 'ntohs()' the second short of the DNS header. This worked before because QR is usually set whenever RA is set, and we probably don't use the other fields.

--- dnstop.c
+++ dnstop.c
@@ -328,6 +328,7 @@ handle_dns(const char *buf, int len)
     qh.id = ntohs(us);
 
     memcpy(&us, buf + 2, 2);
+    us = ntohs(us);
     qh.qr = (us >> 15) & 0x01;
     qh.opcode = (us >> 11) & 0x0F;
     qh.aa = (us >> 10) & 0x01;
