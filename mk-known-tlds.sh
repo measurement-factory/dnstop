07562ea85cd253745108ab0c1e779b4826ff2565
Duane Wessels <wessels@measurement-factory.com>
2006-04-24 16:53:39 +0000

    adding

---
+++ mk-known-tlds.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+fetch -o /tmp/tlds http://data.iana.org/TLD/tlds-alpha-by-domain.txt
+exec </tmp/tlds
+
+cat <<EOF
+
+static const char *KnownTLDS[] = {
+	".",	/* special case for root zone */
+EOF
+
+while read t ; do
+	echo $t | grep -sq '^#' && continue;
+	x=`echo $t | tr A-Z a-z`
+	echo "	\"$x\","
+	if test $x = "arpa" ; then
+		echo "	\"in-addr.arpa\",	 /* because dnstop treats in-addr.arpa as a TLD */"
+	fi
+done
+
+cat <<EOF
+	NULL
+};
+EOF
