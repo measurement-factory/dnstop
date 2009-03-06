f1ad488b5254b544d363c782128fc7a11a66410e
Duane Wessels <wessels@measurement-factory.com>
2009-03-06 18:46:01 +0000

    I find that I need to set pcap_open_live() to_ms parameter very low to get good responsiveness from dnstop on FreeBSD 6.3.  Otherwise it seems packets are buffered for too long and the display doesn't update as quickly as it should.

--- dnstop.c
+++ dnstop.c
@@ -1816,7 +1816,7 @@ main(int argc, char *argv[])
     if (readfile_state) {
 	pcap = pcap_open_offline(device, errbuf);
     } else {
-	pcap = pcap_open_live(device, PCAP_SNAPLEN, promisc_flag, 1000, errbuf);
+	pcap = pcap_open_live(device, PCAP_SNAPLEN, promisc_flag, 1, errbuf);
     }
     if (NULL == pcap) {
 	fprintf(stderr, "pcap_open_*: %s\n", errbuf);
